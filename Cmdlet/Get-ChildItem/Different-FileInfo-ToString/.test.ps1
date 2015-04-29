10ad636ccf0e258f53561eb308d9fd8cfcd9d3e6
Roman Kuzmin <nightroman@gmail.com>
2015-04-29 04:52:30 +0100

    Get-ChildItem -> Get-ChildItem/Different-FileInfo-ToString

--- Cmdlet/Get-ChildItem/Different-FileInfo-ToString/.test.ps1
+++ Cmdlet/Get-ChildItem/Different-FileInfo-ToString/.test.ps1
@@ -3,7 +3,7 @@ task different.FileInfo.ToString {
 	($r = .\different.FileInfo.ToString.ps1)
 
 	# full path
-	assert ($r.strings1[0] -like '*\Get-ChildItem\.test.ps1')
+	assert ($r.strings1[0] -like '*\Different-FileInfo-ToString\.test.ps1')
 
 	# just name
 	assert ($r.strings2[0] -eq '.test.ps1')
