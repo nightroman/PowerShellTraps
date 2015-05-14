c5a96940ae0300c3c48a7a3d592018a2a86fb47c
Roman Kuzmin <nightroman@gmail.com>
2015-05-14 21:35:00 +0100

    ForEach-Object

--- Cmdlets/ForEach-Object/test1.ForEach-Object.bad.error.info.ps1
+++ Cmdlets/ForEach-Object/test1.ForEach-Object.bad.error.info.ps1
@@ -1,12 +1,12 @@
 
 <#
-The code fails in the script "Oops.ps1" but this not clear from the shown error:
+The code fails in the script "Oops.ps1" but this is not clear from the shown error:
 
 	Get-ChildItem : Oops
 	At ...\test1.ForEach-Object.bad.error.info.ps1 ...
 	+ Get-ChildItem | ForEach-Object {
 
-Note that the information looks like Get-ChildItem has failed.
+It looks like Get-ChildItem has failed in "test1.ForEach-Object.bad.error.info.ps1".
 #>
 
 Get-ChildItem | ForEach-Object {
