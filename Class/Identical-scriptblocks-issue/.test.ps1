0f563513e79b0120183e23b6a862237a8446f06e
Roman Kuzmin <nightroman@gmail.com>
2016-07-28 18:09:59 +0100

    Fix test

--- Class/Identical-scriptblocks-issue/.test.ps1
+++ Class/Identical-scriptblocks-issue/.test.ps1
@@ -1,18 +1,19 @@
 
 $Version = $PSVersionTable.PSVersion
 ${5.0} = [version]'5.0'
+${5.1} = [version]'5.1'
 if ($Version -lt ${5.0}) {return task test5}
 
 task Test-1.fails {
 	($r = try {.\Test-1.fails.ps1} catch {$_})
-	if ($Version -eq ${5.0}) {
+	if ($Version -ge ${5.1}) {
+		# weird error "Parameter name: type" with no source
+		equals $r.FullyQualifiedErrorId 'System.ArgumentNullException,Test-1.fails.ps1'
+	}
+	else {
 		equals $r.Count 2
 		assert ($r[0].GetType().FullName -cmatch '^<\w{8}>\.A$')
 		equals $r[1].FullyQualifiedErrorId TypeNotFound
-	}
-	else {
-		# weird error "Parameter name: type" with no source
-		equals $r.FullyQualifiedErrorId 'System.ArgumentNullException,Test-1.fails.ps1'
 	}
 }
 
