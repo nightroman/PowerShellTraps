907404d6ce71f5cb4666a8153ca0830cabed0d4c
Roman Kuzmin <nightroman@gmail.com>
2017-11-10 06:09:49 +0000

    Amend Basic/Number-of-returned-objects/test5.PSCustomObject.ps1

--- Basic/Number-of-returned-objects/test5.PSCustomObject.ps1
+++ Basic/Number-of-returned-objects/test5.PSCustomObject.ps1
@@ -1,8 +1,15 @@
 
+$Version = $PSVersionTable.PSVersion.Major
+if ($Version -ge 3) {
+	Set-StrictMode -Off
+}
+
 $object = [PSCustomObject]@{Name='Joe'; Age=42}
 
 # v2: 2, $false, Hashtable
-# v3+: $null, $true, PSCustomObject
+# v3+:
+# - $null, $true, PSCustomObject
+# - error in strict mode (v5)
 
 $object.Count
 $null -eq $object.Count
