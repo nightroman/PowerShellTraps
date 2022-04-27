d43025ac2a4f576b5f6901b3c6e95b134b45bf00
Roman Kuzmin <nightroman@gmail.com>
2022-04-27 13:34:38 +0100

    Tweak test.

--- Basic/FormatEnumerationLimit/.test.ps1
+++ Basic/FormatEnumerationLimit/.test.ps1
@@ -5,7 +5,7 @@
 task Test-1-local-value-ignored {
 	($r = .\Test-1-local-value-ignored.ps1)
 	if ($PSVersionTable.PSVersion.Major -ge 6) {
-		$r = $r -replace "`e.+?m"
+		$r = $r -replace "`e\[[0-9;]*m"
 		equals 'Data1 : {1, 2, 3, 4…}' $r.Trim()
 	}
 	else {
@@ -16,7 +16,7 @@ task Test-1-local-value-ignored {
 task Test-2-global-value-works {
 	($r = .\Test-2-global-value-works.ps1)
 	if ($PSVersionTable.PSVersion.Major -ge 6) {
-		$r = $r -replace "`e.+?m"
+		$r = $r -replace "`e\[[0-9;]*m"
 		equals 'Data1 : {1, 2, 3, 4, 5, 6, 7, 8…}' $r.Trim()
 	}
 	else {
